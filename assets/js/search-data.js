648d305bdef13c69538113bed6dc38a5202103bc
alexayling <aylingalex001@gmail.com>
2025-11-04 22:25:11 +0000

    Deploying to gh-pages from @ alexayling/alexayling.github.io@4d1ae26c0dfb8d0474131b0893f142fd10965099 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -57,16 +57,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-in-a-first-caltech-s-space-solar-power-demonstrator-wirelessly-transmits-power-in-space",
+          title: 'In a First, Caltech’s Space Solar Power Demonstrator Wirelessly Transmits Power in Space....',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
